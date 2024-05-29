a9acd2ed62387835de0123fd91c246fe08e6d4ff
Rollo250 <sep1678@outlook.com>
2024-05-29 08:07:52 -0300

    2-Curso rapido convex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
-//import ConvexClientProvider from "@/providers/convex-client-provider";
+import ConvexClientProvider from "@/providers/convex-client-providers";
 //import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,10 +21,10 @@ export default function RootLayout({
 		<html lang='en'>
 			<body className={inter.className}>
 				<ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-					{/*<ConvexClientProvider>*/}
+					<ConvexClientProvider>
 						{children}
-						{/*<Toaster />
-					</ConvexClientProvider>*/}
+						{/*<Toaster />*/}
+					</ConvexClientProvider>
 				</ThemeProvider>
 			</body>
 		</html>
