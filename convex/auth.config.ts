415a0b470737d43a1d1f07b21a061c083db2e423
Rollo250 <sep1678@outlook.com>
2024-05-30 07:46:59 -0300

    2-curso rapido clerks-NF

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://relaxing-chimp-36.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
+
