aa3171050e4794ea2e0065801d05407f97700c62
Rollo250 <sep1678@outlook.com>
2024-06-04 18:15:42 -0300

    3.2 guardado

--- convex/users.ts
+++ convex/users.ts
@@ -73,3 +73,41 @@ export const setUserOffline = internalMutation({
         await ctx.db.patch(user._id, {isOnline: false});
     },
 });
+
+export const getUsers = query({
+    args: {
+        tokenIdentifier: v.string()
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if(!identity) {
+            throw new ConvexError("Usuario no autorizado");
+        }
+
+        const user = await ctx.db.query("users").collect();
+        return user;
+    },
+});
+
+export const getMe = query({
+    args:{},
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if(!identity) {
+            throw new ConvexError("No autorizado");
+        }
+
+        const user = await ctx.db
+        .query("users")
+        .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", identity.tokenIdentifier))
+        .unique();
+
+        if (!user) {
+            throw new ConvexError("Usuario no encontrado");
+        }
+
+        return user;
+},
+});
+
+//Pendiente agregar getGroupMembers ---luego
